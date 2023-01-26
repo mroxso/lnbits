9978f28e2df7feea5c17210cc9ca563df488dafb
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-01-26 17:09:20 +0000

    habemos messages

--- lnbits/extensions/market/static/js/utils.js
+++ lnbits/extensions/market/static/js/utils.js
@@ -86,19 +86,21 @@ async function subscribeToChatRelay(relay, pubkeys) {
     console.log(`failed to connect to ${relay.url}`)
   })
 
-  let sub = relay.sub({
-    filter: {
+  let sub = relay.sub([
+    {
       kinds: [4],
-      authors: authors,
-      '#p': authors
+      '#p': pubkeys
     }
-  })
+  ])
 
   sub.on('event', event => {
     console.log('we got the event we wanted:', event)
   })
 
-  return sub
+  sub.on('eose', () => {
+    console.log('unsubscribed from', relay.url)
+    sub.unsub()
+  })
 }
 
 async function publishNostrEvent(relay, event) {
@@ -126,3 +128,121 @@ async function publishNostrEvent(relay, event) {
   })
   return
 }
+
+/*class RelayPool {
+  constructor(relays, opts) {
+    if (!(this instanceof RelayPool)) return new RelayPool(relays, opts)
+    this.relays = []
+    this.opts = opts
+
+    for (const relay of relays) {
+      this.add(relay)
+    }
+
+    return this
+  }
+
+  close() {
+    for (const relay of this.relays) {
+      relay.close()
+    }
+  }
+
+  on(method, fn) {
+    for (const relay of this.relays) {
+      this.onfn[method] = fn
+      relay.onfn[method] = fn.bind(null, relay)
+    }
+    return this
+  }
+
+  has(relayUrl) {
+    for (const relay of this.relays) {
+      if (relay.url === relayUrl) return true
+    }
+
+    return false
+  }
+
+  send(payload, relay_ids) {
+    const relays = relay_ids ? this.find_relays(relay_ids) : this.relays
+    for (const relay of relays) {
+      relay.send(payload)
+    }
+  }
+
+  setupHandlers() {
+    // setup its message handlers with the ones we have already
+    const keys = Object.keys(this.onfn)
+    for (const handler of keys) {
+      for (const relay of this.relays) {
+        relay.onfn[handler] = this.onfn[handler].bind(null, relay)
+      }
+    }
+  }
+
+  remove(url) {
+    let i = 0
+
+    for (const relay of this.relays) {
+      if (relay.url === url) {
+        relay.ws && relay.ws.close()
+        this.relays = this.replays.splice(i, 1)
+        return true
+      }
+
+      i += 1
+    }
+
+    return false
+  }
+
+  subscribe(sub_id, filters, relay_ids) {
+    const relays = relay_ids ? this.find_relays(relay_ids) : this.relays
+    for (const relay of relays) {
+      relay.subscribe(sub_id, filters)
+    }
+  }
+
+  unsubscribe(sub_id, relay_ids) {
+    const relays = relay_ids ? this.find_relays(relay_ids) : this.relays
+    for (const relay of relays) {
+      relay.unsubscribe(sub_id)
+    }
+  }
+
+  add(relay) {
+    if (relay instanceof Relay) {
+      if (this.has(relay.url)) return false
+      this.relays.push(relay)
+      this.setupHandlers()
+      return true
+    }
+
+    if (this.has(relay)) return false
+
+    const r = new Relay(relay, this.opts)
+    this.relays.push(r)
+    this.setupHandlers()
+    return true
+  }
+
+  find_relays(relay_ids) {
+    if (relay_ids instanceof Relay) return [relay_ids];
+  }
+}
+
+RelayPool.prototype.find_relays = function relayPoolFindRelays(relay_ids) {
+  if (relay_ids instanceof Relay) return [relay_ids]
+
+  if (relay_ids.length === 0) return []
+
+  if (!relay_ids[0]) throw new Error('what!?')
+
+  if (relay_ids[0] instanceof Relay) return relay_ids
+
+  return this.relays.reduce((acc, relay) => {
+    if (relay_ids.some(rid => relay.url === rid)) acc.push(relay)
+    return acc
+  }, [])
+}*/
